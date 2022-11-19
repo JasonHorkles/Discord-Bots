5e03cb094b2e5a4f23b93164a90742d7b454a65b
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2022-11-19 13:55:31 -0700

    Add milestone

--- Silverstone/src/main/java/me/jasonhorkles/silverstone/Time.java
+++ Silverstone/src/main/java/me/jasonhorkles/silverstone/Time.java
@@ -17,9 +17,10 @@ public class Time {
             Period p = new Period(new LocalDate(1511420400000L), LocalDate.now());
 
             VoiceChannel channel = Silverstone.jda.getVoiceChannelById(914918234518593546L);
-            channel.getManager().setName(
-                    p.getYears() + "y " + p.getMonths() + "m " + (p.getDays() + (p.getWeeks() * 7)) + "d")
-                .queue();
+            channel.getManager()
+                .setName(p.getYears() + "y " + p.getMonths() + "m " + (p.getDays() + (p.getWeeks() * 7)) + "d").queue();
+
+            if (p.getMonths() == 0 && p.getDays() + (p.getWeeks() * 7) == 0) new Secrets().yearlyMsg(p.getYears());
         }, 0, 6, TimeUnit.HOURS);
     }
 }
