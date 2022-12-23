e55f3642225efdcfcec57e6255286daf6fa3c4e7
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2022-12-22 23:23:26 -0700

    Move traffic scheduler to its own class

--- StormAlerts/src/main/java/me/jasonhorkles/stormalerts/Traffic.java
+++ StormAlerts/src/main/java/me/jasonhorkles/stormalerts/Traffic.java
@@ -2,6 +2,15 @@ package me.jasonhorkles.stormalerts;
 
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Traffic {
     public void checkTraffic(boolean north) {
         int min = 1;
@@ -10,23 +19,27 @@ public class Traffic {
         Thread checks = new Thread(() -> {
             try {
                 for (int section = min; section <= max; section++) {
-                    JSONObject input = new Secrets().getTrafficAtCoords(section).getJSONObject("flowSegmentData");
+                    JSONObject input = new Secrets().getTrafficAtCoords(section)
+                        .getJSONObject("flowSegmentData");
                     if (input != null) {
                         // Check for closure
                         if (input.getBoolean("roadClosure")) {
-                            System.out.println(new Utils().getTime(Utils.LogColor.YELLOW) + new Secrets().getRoadName(
-                                north) + " Traffic section " + section + "/" + max + " is closed!");
+                            System.out.println(
+                                new Utils().getTime(Utils.LogColor.YELLOW) + new Secrets().getRoadName(
+                                    north) + " Traffic section " + section + "/" + max + " is closed!");
                             StormAlerts.jda.openPrivateChannelById(277291758503723010L).flatMap(
-                                channel -> channel.sendMessage(
-                                    "**" + new Secrets().getRoadName(north) + "** is closed! :no_entry:")).queue();
+                                    channel -> channel.sendMessage(
+                                        "**" + new Secrets().getRoadName(north) + "** is closed! :no_entry:"))
+                                .queue();
                             continue;
                         }
 
                         // Get speed
                         int currentSpeed = input.getInt("currentSpeed");
 
-                        System.out.println(new Utils().getTime(Utils.LogColor.GREEN) + new Secrets().getRoadName(
-                            north) + " Traffic section " + section + "/" + max + " is currently " + currentSpeed + " mph");
+                        System.out.println(
+                            new Utils().getTime(Utils.LogColor.GREEN) + new Secrets().getRoadName(
+                                north) + " Traffic section " + section + "/" + max + " is currently " + currentSpeed + " mph");
 
                         int finalSection = section;
                         if (currentSpeed <= 55 && currentSpeed >= 40)
@@ -35,10 +48,11 @@ public class Traffic {
                                         north) + " section " + finalSection + "/" + max + "** has a slowdown @ **" + currentSpeed + " mph**! :yellow_circle:"))
                                 .queue();
 
-                        else if (currentSpeed < 40) StormAlerts.jda.openPrivateChannelById(277291758503723010L).flatMap(
-                                channel -> channel.sendMessage("**" + new Secrets().getRoadName(
-                                    north) + " section " + finalSection + "/" + max + "** has a slowdown @ **" + currentSpeed + " mph**! :red_circle:"))
-                            .queue();
+                        else if (currentSpeed < 40)
+                            StormAlerts.jda.openPrivateChannelById(277291758503723010L).flatMap(
+                                    channel -> channel.sendMessage("**" + new Secrets().getRoadName(
+                                        north) + " section " + finalSection + "/" + max + "** has a slowdown @ **" + currentSpeed + " mph**! :red_circle:"))
+                                .queue();
                     }
 
                     try {
@@ -54,9 +68,29 @@ public class Traffic {
                 e.printStackTrace();
 
                 StormAlerts.jda.openPrivateChannelById(277291758503723010L)
-                    .flatMap(channel -> channel.sendMessage("**Failed to check traffic!** :warning:")).queue();
+                    .flatMap(channel -> channel.sendMessage("**Failed to check traffic!** :warning:"))
+                    .queue();
             }
         }, "Traffic Check");
         checks.start();
+    }
+
+    public void scheduleTrafficCheck(String time, boolean toWork) throws ParseException {
+        DayOfWeek day = LocalDate.now().getDayOfWeek();
+        if (day != DayOfWeek.SATURDAY && day != DayOfWeek.SUNDAY) {
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd h:mm a");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(format.parse(LocalDate.now() + " " + time));
+
+            long delay = calendar.getTimeInMillis() - System.currentTimeMillis();
+
+            if (delay >= 0) {
+                StormAlerts.scheduledTimers.add(Executors.newSingleThreadScheduledExecutor()
+                    .schedule(() -> new Traffic().checkTraffic(toWork), delay, TimeUnit.MILLISECONDS));
+                System.out.println(
+                    new Utils().getTime(Utils.LogColor.GREEN) + "Scheduled traffic check in " + Math.round(
+                        delay / 3600000.0) + " hours.");
+            }
+        }
     }
 }
