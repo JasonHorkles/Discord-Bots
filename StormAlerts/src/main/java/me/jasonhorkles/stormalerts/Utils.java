50d001b63f149f6067900168535962842e38428b
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2022-12-31 17:59:18 -0700

    Increase no-ping time to 2 hours

--- StormAlerts/src/main/java/me/jasonhorkles/stormalerts/Utils.java
+++ StormAlerts/src/main/java/me/jasonhorkles/stormalerts/Utils.java
@@ -89,8 +89,8 @@ public class Utils {
         try {
             Message message = new Utils().getMessages(channel, 1).get(30, TimeUnit.SECONDS).get(0);
             if (message.isEdited()) //noinspection DataFlowIssue
-                return message.getTimeEdited().isBefore(OffsetDateTime.now().minus(1, ChronoUnit.HOURS));
-            else return message.getTimeCreated().isBefore(OffsetDateTime.now().minus(1, ChronoUnit.HOURS));
+                return message.getTimeEdited().isBefore(OffsetDateTime.now().minus(2, ChronoUnit.HOURS));
+            else return message.getTimeCreated().isBefore(OffsetDateTime.now().minus(2, ChronoUnit.HOURS));
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             System.out.print(new Utils().getTime(LogColor.RED));
             e.printStackTrace();
