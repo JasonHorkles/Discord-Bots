dd48018bf115dffa218de757d3e7ebbc424de165
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2024-04-08 12:58:07 -0600

    Add auto publish to announcements

--- Polytrichopsida/src/main/java/me/jasonhorkles/polytrichopsida/Events.java
+++ Polytrichopsida/src/main/java/me/jasonhorkles/polytrichopsida/Events.java
@@ -78,6 +78,14 @@ public class Events extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        // Auto publish announcements in updates & github-spam
+        if (event.getChannel().getIdLong() == 1226930147245035571L || event.getChannel()
+            .getIdLong() == 1226929485895434271L) {
+            event.getMessage().crosspost().queue(null,
+                new ErrorHandler().ignore(ErrorResponse.UNKNOWN_MESSAGE, ErrorResponse.ALREADY_CROSSPOSTED));
+            return;
+        }
+
         if (event.getAuthor().isBot()) return;
 
         // Plugin support thread
