a2a60a19f6ea84342ca50bd48be8906a06e54877
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2024-06-13 21:22:44 -0600

    Shut up if recent live notif

--- Silverstone/src/main/java/me/jasonhorkles/silverstone/Live.java
+++ Silverstone/src/main/java/me/jasonhorkles/silverstone/Live.java
@@ -3,10 +3,14 @@ package me.jasonhorkles.silverstone;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
+import java.time.OffsetDateTime;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Live {
     private static Message liveMessage;
@@ -37,17 +41,27 @@ public class Live {
         return activity;
     }
 
+    @SuppressWarnings("DataFlowIssue")
     private void sendLiveAlert(Activity activity) {
         EmbedBuilder embed = new EmbedBuilder();
         embed.setImage("https://static-cdn.jtvnw.net/previews-ttv/live_user_jasonhorkles-1920x1080.jpg?id=" + System.currentTimeMillis() / 1000);
         embed.setColor(new Color(43, 45, 49));
 
-        //noinspection DataFlowIssue
-        liveMessage = Silverstone.jda.getGuildById(455919765999976461L)
-            .getNewsChannelById(847942135402594314L)
-            .sendMessage("Jason is now live in **" + activity.getState() + "**!\nVisit the stream at [**twitch.tv/jasonhorkles**](https://twitch.tv/jasonhorkles)\n<@&1196208254804566027>")
-            .complete();
+        NewsChannel channel = Silverstone.jda.getGuildById(455919765999976461L).getNewsChannelById(
+            847942135402594314L);
+        String ping = "\n<@&1196208254804566027>";
 
-        liveMessage.editMessageEmbeds(embed.build()).queueAfter(10, TimeUnit.MINUTES);
+        try {
+            if (new Utils().getMessages(channel, 1).get(30, TimeUnit.SECONDS).getFirst().getTimeCreated()
+                .isAfter(OffsetDateTime.now().minusHours(1))) ping = "";
+
+            liveMessage = channel
+                .sendMessage("Jason is now live in **" + activity.getState() + "**!\nVisit the stream at [**twitch.tv/jasonhorkles**](https://twitch.tv/jasonhorkles)" + ping)
+                .setSuppressedNotifications(ping.isEmpty()).complete();
+
+            liveMessage.editMessageEmbeds(embed.build()).queueAfter(10, TimeUnit.MINUTES);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
