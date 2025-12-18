7b3ee4de47e334819158116fa59863f539255356
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2025-12-17 17:26:17 -0700

    Update JDA

--- Booper/src/main/java/me/jasonhorkles/booper/events/LiveDiscord.java
+++ Booper/src/main/java/me/jasonhorkles/booper/events/LiveDiscord.java
@@ -20,7 +20,6 @@ public class LiveDiscord extends ListenerAdapter {
     public void onUserUpdateActivities(@NotNull UserUpdateActivitiesEvent event) {
         if (event.getGuild().getIdLong() != 1299547538445307986L) return;
         if (event.getUser().getIdLong() == 314933806173913089L) return;
-        if (event.getUser().getIdLong() == 321852845144145920L) return;
         checkIfLive(event.getMember());
     }
 
